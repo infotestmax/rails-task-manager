e0432d32b18889e48d789c75eea6c3017487f2ed
Jean Michel YIP <info.test@free.fr>
2021-11-09 15:58:32 +0100

    Tasks edit update delete Ok

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,15 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get 'tasks', to: 'tasks#index'
   get 'tasks/:id', to: 'tasks#show', as: 'task'
+
+  # Create
+  get 'task/new', to: 'tasks#new'
+  post 'tasks', to: 'tasks#create'
+
+  # Update
+  get 'tasks/:id/edit', to: 'tasks#edit', as: 'task_edit'
+  patch 'tasks/:id', to: 'tasks#update'
+
+  # Delete
+  delete 'tasks/:id', to: 'tasks#destroy', as: 'task_delete'
 end
